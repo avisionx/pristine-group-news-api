0e469e1cf069529cc16e88fe61680688f94ccfb2
Avi Garg <avi.grg4567@gmail.com>
2023-10-11 17:13:03 +0530

    fix urls for href and scraping

--- main.py
+++ main.py
@@ -8,45 +8,42 @@ import json
 import datetime
 
 
+HOST_URL = "https://realty.economictimes.indiatimes.com"
+SCRAPE_URL = HOST_URL + "/latest-news"
+
+
 def getNewsJson(data):
     return {
-        'title': data.h3.text,
-        'para': data.p.text,
-        'href': data.a['href'],
-        'img': 'data:image/png;base64, ' + base64.b64encode(requests.get(data.img['data-src']).content).decode('utf-8'),
-        'img-src': data.img['data-src'],
+        "title": data.h3.text,
+        "para": data.p.text,
+        "href": HOST_URL + data.a["href"],
+        "img": "data:image/png;base64, "
+        + base64.b64encode(requests.get(data.img["data-src"]).content).decode("utf-8"),
+        "img-src": data.img["data-src"],
         # 'tag': data.span.text.title()
     }
 
 
-URL = 'https://realty.economictimes.indiatimes.com/latest-news'
-
-r = requests.get(URL)
-soup = BeautifulSoup(r.content, 'html5lib')
-dataItems = soup.find_all('article', {'class': 'desc'})
+r = requests.get(SCRAPE_URL)
+soup = BeautifulSoup(r.content, "html5lib")
+dataItems = soup.find_all("article", {"class": "desc"})
 
 now = datetime.datetime.now()
-date = {key: val for key, val in zip(['d', 'm', 'Y'], now.strftime('%d/%m/%Y').split("/"))}
-time = {key: val for key, val in zip(['H', 'M', 'S'], now.strftime('%H:%M:%S').split(":"))}
+date = {
+    key: val for key, val in zip(["d", "m", "Y"], now.strftime("%d/%m/%Y").split("/"))
+}
+time = {
+    key: val for key, val in zip(["H", "M", "S"], now.strftime("%H:%M:%S").split(":"))
+}
 
 newsDump = []
 for news in dataItems:
     newsDump.append(getNewsJson(news))
 
-with open('news.json', 'w') as outfile:
-    json.dump({
-        'updated_at': {
-            'date': date,
-            'time': time
-        },
-        'news': newsDump
-    }, outfile)
-    
-with open('news-top.json', 'w') as outfile:
-    json.dump({
-        'updated_at': {
-            'date': date,
-            'time': time
-        },
-        'news': newsDump[:3]
-    }, outfile)
+with open("news.json", "w") as outfile:
+    json.dump({"updated_at": {"date": date, "time": time}, "news": newsDump}, outfile)
+
+with open("news-top.json", "w") as outfile:
+    json.dump(
+        {"updated_at": {"date": date, "time": time}, "news": newsDump[:3]}, outfile
+    )
